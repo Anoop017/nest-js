946ce3cfb08b8db65cf75a0d74aa1f7d368cc50b
Anoop017 <anoopsnair1123@gmail.com>
2026-02-01 11:01:20 +0530

    Added 2 new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { NamesModule } from './names/names.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, NamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
