636ef28e855247d71619b09e3fedda13a6049548
Anoop017 <anoopsnair1123@gmail.com>
2026-02-14 22:02:14 +0530

    Added something because i am using company Github now to push

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service'
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller()
 export class UsersController {
     constructor(private readonly userService:
         UsersService) { }
 
-    @Get()
-    findAll() {
+    @Get('users01')
+    findAllUsers() {
+        return this.userService.findAll();
+    }
+
+    @Get('userName')
+    findAllByUserName() {
+        return this.userService.findAll();
+    }
+
+    @Get('Login')
+    findAllByLogin() {
         return this.userService.findAll();
     }
 
